44e7da45c6507616cd211a9d3514e95a0ea56bf2
MaksMusic <Maksmusic05@yandex.ru>
2022-12-12 00:14:50 +0300

    replaceAll

--- src/regular_b.java
+++ src/regular_b.java
@@ -1,22 +1,24 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class regular_b {
     public static void main(String[] args) {
-
-
-
-        String text = "Hello joni wtas are you Joni";
         Scanner scanner = new Scanner(System.in);
-        String text2 = scanner.nextLine();
-        String str2 = text2.replaceAll("[Сс]оч[иИ]","ростов"); //замена текста во всем тексте
+        String text = scanner.nextLine();
+        Pattern pattern = Pattern.compile("^Joni$");//создать правило
+        Matcher matcher = pattern.matcher(text);//проверка
+        System.out.println(matcher.matches());//получение результата проверки
+        /////////////
 
-        System.out.println(str2);
 
 
 
-        System.out.println(text.replaceAll("[jJ]oni","Tomi")); //замена текста во всем тексте
+        String text2 = "Привет Анна ты из Россия ?  ";
+        //поиск текса с разными вариантами Россия России россия россии
+        System.out.println(text2.replaceAll("[Ро]осси[ия]","Канады")); //замена текста во всем тексте
 
 
     }
