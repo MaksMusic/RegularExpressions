879b4236db2b8ebc87f72a10477df006023117d5
MaksMusic <Maksmusic05@yandex.ru>
2023-10-03 21:04:38 +0300

    regex

--- src/Regex6.java
+++ src/Regex6.java
@@ -42,10 +42,12 @@ class Employee{
     double bonus;
 
     public Employee(int id, String name, String surname, int salary, double bonus) {
+
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.salary = salary;
         this.bonus = bonus;
+
     }
 }
